caf856ebc0ec02a30e66cab1242cbce2d681d8db
Sofia Jonsson <jonssonsofia93@gmail.com>
2018-12-22 20:26:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = { };
 
 function updateObjectWithKeyAndValue(object, key, value){
  object [key]=value;
-  return recipes.concat[object];
+  return recipes.concat[];
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
